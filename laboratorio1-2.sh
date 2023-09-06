8f818fb9f1b150c8b3e2a9c01abe42b4f6d98e40
crn15 <candyrojas579@gmail.com>
2023-09-05 20:14:02 -0600

    SEGUNDA PARTE TERMINADA.

--- laboratorio1-2.sh
+++ laboratorio1-2.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+
+function group_permissions()
+{
+	echo "EL USUARIO $USER SE AGREGARÁ AL GRUPO $GROUP."
+	sudo adduser $USER $GROUP
+	echo "EL USUARIO DEFAULT SE AGREGARÁ AL GRUPO $GROUP."
+	user_default=$(grep :$(id -u): /etc/passwd | cut -d: -f1)
+	sudo adduser $user_default $GROUP
+	echo "SE LE DARÁN PERMISOS DE EJECUACIÒN AL GRUPO $GROUP SOBRE EL ARCHIVO laboratorio1.sh"
+	sudo chown :$GROUP laboratorio1.sh
+	sudo chmod 676 laboratorio1.sh
+	echo "AHORA SOLO EL GRUPO $GROUP TIENE PERMISO DE EJECUACIÒN SOBRE EL ARCHIVO laboratorio1.sh"
+}
+
+
+
 read -p "INGRESE EL NOMBRRE DEL USUARIO: " USER
 
 USERS=$(ls /home)
@@ -15,9 +31,9 @@ done
 
 if [ $exist -eq 0 ]; then
         sudo useradd -m $USER
-        echo "Usuario creado."
+        echo "USUARIO CREADO."
 else
-        echo "El usuario ya existe."
+        echo "USUARIO YA EXISTE."
 
 fi
 
@@ -25,23 +41,25 @@ fi
 
 read -p "INGRESE EL NOMBRE DEL GRUPO: " GROUP
 
-GROUPS=$(cat /etc/group)
-existe=0
-echo $GROUPS
+mapfile -t GRUPOS < <(cut -d: -f1 /etc/group)
+found=0
 
-for group in $GROUPS
+for group in "${GRUPOS[@]}"
 do
-        if [ $GROUP == $group ]; then
-                existe=1
+	if [ $GROUP == $group ]; then
+                found=1
                 break
         fi
+
 done
 
-if [ $existe -eq 0 ]; then
-        sudo groupadd  $GROUP
-        echo "Grupo creado."
+if [ $found -eq 0 ]; then
+        sudo groupadd $GROUP
+        echo "GRUPO CREADO."
 else
-        echo "El Grupo ya existe."
+        echo "GRUPO YA EXISTE."
 
 fi
+
+group_permissions
 
